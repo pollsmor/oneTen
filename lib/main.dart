88caf73ae781cb43c820dac48755d15205435d25
pollsmor <kevinli4321@gmail.com>
2019-06-29 17:46:40 -0400

    Bottom navbar items

--- lib/main.dart
+++ lib/main.dart
@@ -7,57 +7,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      title: 'oneTen',
+      home: Scaffold(
+        body: Center(
+          child: Text("oneTen"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              title: Text("Favorites"),
+              icon: Icon(Icons.favorite_border),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            BottomNavigationBarItem(
+              title: Text("Games"),
+              icon: Icon(Icons.videogame_asset),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Settings"),
+              icon: Icon(Icons.settings),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
