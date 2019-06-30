9b9afa733e250c9cbaf862962871ff5862a67776
pollsmor <kevinli4321@gmail.com>
2019-06-30 10:23:38 -0400

    Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,56 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'oneTen',
-      home: Scaffold(
-        body: Center(
-          child: Text("oneTen"),
+      home: FavoritesPage(),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        leading: Icon(
+          Icons.search,
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              title: Text("Favorites"),
-              icon: Icon(Icons.favorite_border),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Games"),
-              icon: Icon(Icons.videogame_asset),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Settings"),
-              icon: Icon(Icons.settings),
-            ),
-          ],
+        title: Text(
+          "Search for a game",
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.white70,
+          ),
         ),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("oneTen"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Favorites"),
+            icon: Icon(Icons.favorite_border),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Games"),
+            icon: Icon(Icons.videogame_asset),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Settings"),
+            icon: Icon(Icons.settings),
+          ),
+        ],
       ),
     );
   }
