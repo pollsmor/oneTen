77ad59c402be43f809b5f1dc6663b7498e622500
pollsmor <kevinli4321@gmail.com>
2019-07-11 19:16:15 -0400

    Just realized I haven't git added all the latest runs stuff

---
+++ lib/LatestRunsPage.dart
@@ -0,0 +1,30 @@
+import "package:flutter/material.dart";
+import "API.dart";
+import "main.dart";
+
+class LatestRunsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<LatestRun>>(
+        future: MyApp.latestRuns,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(snapshot.data[index].comment ?? ""),
+                );
+              }
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          return CircularProgressIndicator();
+        }
+      ),
+    );
+  }
+}
