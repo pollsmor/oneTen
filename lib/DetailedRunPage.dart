1a1a324b0e94182cd47bec1feaaa62858d126a05
pollsmor <kevinli4321@gmail.com>
2019-08-09 09:34:50 -0400

    Screw it, just using webview for video playback.

--- lib/DetailedRunPage.dart
+++ lib/DetailedRunPage.dart
@@ -1,64 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import 'API.dart';
-
-class VideoPlayerScreen extends StatefulWidget {
-  VideoPlayerScreen({Key key}) : super(key: key);
-
-  @override
-  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
-}
-
-class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
-
-  @override
-  void initState() {
-    _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
-
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      onTap: () {
-        setState(() {
-          if (_controller.value.isPlaying) {
-            _controller.pause();
-          } else {
-            _controller.play();
-          }
-        });
-      },
-    );
-  }
-}
 
 class DetailedRunPage extends StatelessWidget {
   final String gameName;
@@ -106,7 +49,14 @@ class DetailedRunPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             print(snapshot.data.runs[runIndex].videoLinks);
-            return VideoPlayerScreen();
+            //Twitch: 8 / 7 aspect ratio
+            return AspectRatio(
+              aspectRatio: 1,
+              child: WebView(
+                initialUrl: snapshot.data.runs[runIndex].videoLinks[0],
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+            );
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
